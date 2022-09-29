5dca02f26f4eac28e2f5abe83b8ff94ef242600f
1Gireesh <gireeshbhat68@gmail.com>
2022-09-29 09:53:54 +0530

    added day-2 again

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,21 @@
+import { Box, Center, Container, Flex, Text } from '@chakra-ui/react'
 import React from 'react'
 import "../css/Home.css"
+import Page1Box from './page1Box'
 export default function Home() {
   return (
     <div className='homes'>
-        <h2>home</h2>
+      <div className='pg1'>
+        <Center>
+          <Text fontSize="7xl" w="700px">A platform built for a new way of working</Text>
+        </Center>
+        <Center>
+          <Text fontSize="20px" w="550px">what would you like to manage with monday.com Work OS</Text>
+        </Center>
+        <Flex>
+          <Page1Box></Page1Box>
+        </Flex>
+      </div>
     </div>
   )
 }
---
+++ src/pages/page1Box.jsx
@@ -0,0 +1,10 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+export default function Page1Box() {
+  return (
+    <Box className='page1Box'>
+        
+    </Box>
+  )
+}
